31548a2f31c60f09b9a9752cbe754a5c5df93e15
ThalesFortes <thales_pro@outlook.com>
2025-05-30 15:49:55 -0300

    Formatando para o modelo real

--- scripts.js
+++ scripts.js
@@ -1,9 +1,24 @@
 // Seleciona os elementos do formulário.
-
 const amount = document.getElementById("amount");
 
+// Captura o evento de input para formatar o valor.
 amount.oninput = () => {
+  // Obtém o valor atual do input e remove os caracteres não numéricos.
   let value = amount.value.replace(/\D/g,"")
   
-  amount.value = value
+  value = value / 100
+  
+  // Atualiza o valor do input.
+  amount.value = formatCurrencyBRL(value)
+}
+
+
+function formatCurrencyBRL(value){
+  // Formata o valor no padrão BRL (Real Brasileiro).
+  value = value.toLocaleString("pt-BR",{
+    style:"currency",
+    currency:"BRL",
+  })
+
+  return value
 }
