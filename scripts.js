b5d49b2b36eb03a8264ea80063650876ca7a3506
ThalesFortes <thales_pro@outlook.com>
2025-05-30 15:18:07 -0300

    Permitindo entrada de somente  numeros

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// Seleciona os elementos do formulário.
+
+const amount = document.getElementById("amount");
+
+amount.oninput = () => {
+  let value = amount.value.replace(/\D/g,"")
+  
+  amount.value = value
+}
